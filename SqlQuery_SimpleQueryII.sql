d041b496a9d5c9263fa618e41d012b2d6629ee29
SCARLET GONZALEZ <scgonzalez@powerplan.com>
2021-09-05 17:28:34 -0400

    ExerciseIIandIII

--- SqlQuery_SimpleQueryII.sql
+++ SqlQuery_SimpleQueryII.sql
@@ -1,4 +1,5 @@
-﻿--Query the NAME field for all American cities in the CITY 
+﻿-- Exercise III
+--Query the NAME field for all American cities in the CITY 
 --table with populations larger than 120000. The CountryCode for America is USA.
 
 /*
@@ -7,3 +8,10 @@ Please append a semicolon ";" at the end of the query and enter your query in a 
 */
 
 SELECT NAME FROM CITY WHERE POPULATION > 120000 AND COUNTRYCODE= 'USA';
+
+--Exercise III
+--Query all columns (attributes) for every row in the CITY table. 
+
+SELECT * FROM CITY; 
+
+-- 
