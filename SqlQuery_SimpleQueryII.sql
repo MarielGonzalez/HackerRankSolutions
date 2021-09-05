f121fc93b1c131e614c9897f0ab63bf7a8bdd7a4
SCARLET GONZALEZ <scgonzalez@powerplan.com>
2021-09-05 16:53:39 -0400

    Exercise2

---
+++ SqlQuery_SimpleQueryII.sql
@@ -0,0 +1,9 @@
+﻿--Query the NAME field for all American cities in the CITY 
+--table with populations larger than 120000. The CountryCode for America is USA.
+
+/*
+Enter your query here.
+Please append a semicolon ";" at the end of the query and enter your query in a single line to avoid error.
+*/
+
+SELECT NAME FROM CITY WHERE POPULATION > 120000 AND COUNTRYCODE= 'USA';
