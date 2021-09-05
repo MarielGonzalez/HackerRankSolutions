32b806f67daa47a5673084f84c8781a371800b08
SCARLET GONZALEZ <scgonzalez@powerplan.com>
2021-09-05 16:49:02 -0400

    SelectQuery1

---
+++ SQLQuery_AdvanceJoin.sql
@@ -0,0 +1,10 @@
+﻿--Title; Query Select / Basic/ Easy 
+
+/* Query all columns for all American cities in the CITY 
+table with populations larger than 100000. The CountryCode for America is USA.*/
+
+--My Solution ; 
+ SELECT * FROM  CITY WHERE POPULATION >100000 AND COUNTRYCODE = 'USA'
+
+
+
