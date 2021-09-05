1c3989d854fbf4f462423fd678f3ffb23683c0f4
SCARLET GONZALEZ <scgonzalez@powerplan.com>
2021-09-05 16:37:04 -0400

    QueryFinalSolution

--- SqlQuery_Aggreation.sql
+++ SqlQuery_Aggreation.sql
@@ -1,8 +1,10 @@
 ﻿--Exercise; Weather Observation Station 20/ Aggregation/ Intermediate 
 --Description of the query; A median is defined as a number separating the higher half of a data set from the lower half. Query the median of the Northern Latitudes (LAT_N) from STATION and round your answer to  decimal places.
 --Idea 1st
-SELECT TOP 1 M.LAT_N FROM
-(SELECT TOP(((SELECT COUNT(LAT_N) FROM STATION) + 1)/2) LAT_N FROM STATION ORDER BY LAT_N DESC) AS M
+
+SELECT CAST(MAX(MEDIAN.LAT_N) AS DECIMAL (10,4))
+FROM
+   (SELECT TOP(((SELECT COUNT(LAT_N) FROM STATION) + 1)/2) LAT_N FROM STATION ORDER BY LAT_N ASC) AS MEDIAN
 
 
 
